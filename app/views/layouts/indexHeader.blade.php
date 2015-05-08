9d09e11d406a49b391c0fca572fa1d56dcf43687
rockmetoo <rockmetoo@gmail.com>
2015-05-09 00:37:31 +0900

    responsive index page

--- app/views/layouts/indexHeader.blade.php
+++ app/views/layouts/indexHeader.blade.php
@@ -21,7 +21,7 @@
     </head>
     <body>
         <div id="wrapper">
-            <header class="navbar navbar-fixed-top bg-white" style="border-bottom: 1px solid #cccccc">
+            <nav class="navbar navbar-default navbar-fixed-top bg-white" role="navigation" style="margin-bottom: 0;border-bottom: 1px solid #cccccc">
                 <div class="container-fluid">
                     <div class="navbar-header">
                         <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
@@ -48,7 +48,7 @@
                         </ul>
                     </nav>
                 </div>
-            </header>
+            </nav>
             @yield('content')
             @yield('footer')
         </div>
--- lib/capistrano/tasks/fix_permissions.rake
+++ lib/capistrano/tasks/fix_permissions.rake
@@ -5,6 +5,7 @@ namespace :deploy do
             execute :sudo, :chmod, '-R 0777', current_path.join('app/storage')
             execute :sudo, :chown, '-R nginx:nginx', current_path.join('app/storage');
             execute :sudo, :chown, '-R nginx:nginx', current_path.join('app/storage/meta');
+            execute :sudo, :chown, 'nginx:nginx', current_path.join('app/storage/meta/services.json');
             execute :sudo, :chown, '-R nginx:nginx', current_path.join('public/uploadFiles');
         end
     end
