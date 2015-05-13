84de5b71dc5da3e58de2bd8ad21f46c2d4b8f3c5
Tanvir Shahid <tanvir.shahid@shopairlines.com>
2015-05-13 17:30:05 +0900

    update footer

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -32,18 +32,12 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-3 padding-md">
-                    <p class="font-lg text-danger">Information</p>
-                    <ul class="list-unstyled useful-link">
-                        <li><a href="/about"><small>About Us</small></a></li>
-                        <li><a href="/privacy"><small>Privacy Policy</small></a></li>
-                        <li><a href="/terms"><small>Terms & Conditions</small></a></li>
-                        <li><a href="/sitemap"><small>Site Map</small></a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-3 padding-md">
-                    <p class="font-lg text-danger">Stay Connect</p>
-                    <a href="https://www.facebook.com/akajoho" data-toggle="tooltip" data-original-title="facebook" style="cursor:pointer" target="_blank"><i class="fa fa-facebook"></i></a>
-                    <a href="/subscribe" class="btn" data-toggle="tooltip" data-original-title="email us"  style="cursor:pointer"><i class="fa fa-envelope fa-fw">&nbsp;Subscribe</i></a>
+                        <a href="/about" style="color:white"><small>About Us</small></a>
+                        <a href="/privacy" style="color:white"><small>Privacy Policy</small></a>
+                        <a href="/terms" style="color:white"><small>Terms & Conditions</small></a>
+                        <a href="/sitemap" style="color:white"><small>Site Map</small></a>
+                        <a href="https://www.facebook.com/akajoho" data-toggle="tooltip" data-original-title="facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                    	<a href="/subscribe" class="btn" data-toggle="tooltip" data-original-title="email us" style="color:white"><i class="fa fa-envelope fa-fw">&nbsp;Subscribe</i></a>
                 </div>
             </div>
         </div>
