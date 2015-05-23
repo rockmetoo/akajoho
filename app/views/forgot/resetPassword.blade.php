6b871bc2a41727c1fedc5ed7e3728314bfbfd6be
rockmetoo <rockmetoo@gmail.com>
2015-05-24 02:51:59 +0900

    fix forgot password template

--- app/views/forgot/resetPassword.blade.php
+++ app/views/forgot/resetPassword.blade.php
@@ -55,3 +55,20 @@
         </div>
     </div>
 @stop
+
+@section('footer')
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-3 padding-md">
+                        <a href="/about" style="color:white"><small>About Us</small></a>
+                        <a href="/privacy" style="color:white"><small>Privacy Policy</small></a>
+                        <a href="/terms" style="color:white"><small>Terms & Conditions</small></a>
+                        <a href="/sitemap" style="color:white"><small>Site Map</small></a>
+                        <a href="https://www.facebook.com/akajoho" data-toggle="tooltip" data-original-title="facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                    	<a href="/subscribe" class="btn" data-toggle="tooltip" data-original-title="email us" style="color:white"><i class="fa fa-envelope fa-fw">&nbsp;Subscribe</i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+@stop
